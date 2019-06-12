1350b17969a14fdf4fe8c7464e2e3dbbb2c50f30
Filip Jankovikj <jankovikjfilip@gmail.com>
2019-06-12 22:05:53 +0200

    Cas 1

---
+++ c1/index.js
@@ -0,0 +1,23 @@
+console.log("Kaj si reri");
+
+ime="Petar";
+var prezime="Pejoski";
+let vozrast=16;
+
+console.log(ime);
+
+console.log(`Prezimeto e ${prezime} 007`);
+
+function test(){
+    console.log(ime);
+    console.log(prezime);
+    console.log(vozrast);
+    a=10;
+    var b=20;
+    let c=30;
+}
+test();
+
+console.log(a);
+console.log(b);//ne raboti
+console.log(c);//ne raboti
