a684e85dfc5ec3df235a7a590855b6690edd36f9
Filip Jankovikj <jankovikjfilip@gmail.com>
2019-06-17 22:06:59 +0200

    C2

---
+++ c2/v1.js
@@ -0,0 +1,35 @@
+// var i=0;
+// var b=0;
+// while(i<=100){
+//     if(i%2==0){
+//         b++;
+//     }
+//     if(i%2==0  && b==3){
+//         console.log(i);
+//         b=0;
+//     }
+    
+//     i++;
+// }
+// var n=4;
+// function everyNeven(c){
+//     var i=0;
+//     var b=0;
+//     while(i<=100){
+//         i++;
+//         if(i%2==0){
+//             b++;
+//         }
+//         if(i%2==0 && b==c){
+//             console.log(i);
+//             b=0;
+//         }
+
+
+//     }
+// }
+// everyNeven(n);
+var func= require('./v2');
+func.everyNeven(2);
+var r=func.arrSum([248,21,95,239,46]);
+console.log(r);
