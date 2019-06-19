8fc83cdfaf7e743566bd7a89c57415c5f7cdaa93
petarpejo32 <49168188+petarpejo32@users.noreply.github.com>
2019-06-19 17:28:19 +0200

    Domasno 1 nodejs

---
+++ c2d/domasno2.js
@@ -0,0 +1,37 @@
+var studenti=[
+{ime: "Pero", prezime:"Perovski", prosek: 6.2},
+{ime: "Janko", prezime:"Jankovski", prosek: 7.1},
+{ime: "Stanko", prezime:"Stankovski", prosek: 8.3},
+{ime: "Petko", prezime:"Petkovski", prosek: 9.81},
+{ime: "Ivana", prezime:"Ivanovska", prosek: 6},
+{ime: "Gordana", prezime:"Gordanovska", prosek: 6.1},
+{ime: "Cveta", prezime:"Cvetkoska", prosek: 9.2}
+];
+function func(niza){
+    var zbir=0;
+    var najmal=10;
+    var najgolem=0;
+    for(var i=0;i<niza.length;i++){
+        if(niza[i].prosek>najgolem){
+            najgolem=niza[i].prosek; 
+        }
+        else if(niza[i].prosek<najmal){
+            najmal=niza[i].prosek;
+        }
+        zbir+=niza[i].prosek;
+    }
+
+    var prosek=Math.round(zbir/niza.length*100)/100;
+    console.log("Prosekot e: "+prosek);
+
+    for(var j=0;j<niza.length;j++){
+        if(niza[j].prosek==najgolem){
+            console.log("Najgolem prosek ima: "+niza[j].ime+ " "+ niza[j].prezime);
+        }
+        else if(niza[j].prosek==najmal){
+            console.log("Najmal prosek ima: "+ niza[j].ime+" "+ niza[j].prezime);
+        }
+
+    }
+}
+func(studenti);
