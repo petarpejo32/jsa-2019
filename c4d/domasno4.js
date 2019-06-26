f3bfdf7f06bbe9261ec8111538685ddcd2a62738
petarpejo32 <49168188+petarpejo32@users.noreply.github.com>
2019-06-26 15:38:00 +0200

    domasno4.js quick fix

--- c4d/domasno4.js
+++ c4d/domasno4.js
@@ -18,6 +18,9 @@ var server=http.createServer(function(req,res){
             else if(URL[i]=="/"){
                 output=parseInt(URL[i-1])/parseInt(URL[i+1]);
             }
+            else if(URL[i]=="="){
+                break;
+            }
         }
     }
     //proveruva edno po edno pa ne vazi za prioritet na zagradi i na mnozenje, so uste eden if moze da se sredi ama na trening sum :D
