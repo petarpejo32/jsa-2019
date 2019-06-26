c910c3b252f4fc57c9373addbc7a8ab29185a418
petarpejo32 <49168188+petarpejo32@users.noreply.github.com>
2019-06-26 15:36:49 +0200

    domasno4.js

---
+++ c4d/domasno4.js
@@ -0,0 +1,34 @@
+var http=require('http');
+
+var server=http.createServer(function(req,res){
+    console.log(req);
+    var URL=req.url.split('/');
+    var output=0;
+    for(var i = 0; i<URL.length;i++){
+        if(i%2!=0){
+            if(URL[i]=="+"){
+                output=parseInt(URL[i-1])+parseInt(URL[i+1]);
+            }
+            else if(URL[i]=="-"){
+                output=parseInt(URL[i-1])-parseInt(URL[i+1]);
+            }
+            else if(URL[i]=="*"){
+                output=parseInt(URL[i-1])*parseInt(URL[i+1]);
+            }
+            else if(URL[i]=="/"){
+                output=parseInt(URL[i-1])/parseInt(URL[i+1]);
+            }
+        }
+    }
+    //proveruva edno po edno pa ne vazi za prioritet na zagradi i na mnozenje, so uste eden if moze da se sredi ama na trening sum :D
+    //(ne e proveren kodot samo e dignat serverot no time sorry)
+    res.write(output+'');
+    res.end();
+});
+server.listen(8080,(err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("Success!");
+});
